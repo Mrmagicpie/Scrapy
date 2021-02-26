dd7fa41b1456fc1a1f4c254c01b7b245dcdb2803
Mrmagicpie <realmrmagicpie@gmail.com>
2021-02-26 12:27:03 -0500

    Create scraper.py

---
+++ OhNo/scraper.py
@@ -0,0 +1,65 @@
+#
+#                             OhNo scraper.py | 2021 (c) Mrmagicpie
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#
+import scrapy, time
+from pprint import PrettyPrinter
+#
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#
+class OhNoScraper(scrapy.Spider):
+
+    pp         = PrettyPrinter(indent=4)
+    name       = "ohno"
+    start_urls = [input("Enter your URL: "),]
+    count      = 1
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def parse(self, response):
+        sel = scrapy.Selector(response)
+        results = sel.xpath("//a[contains(@href, '')]/@href")
+        for result in results:
+            href = result.extract()
+            if href.startswith("#"): continue
+            elif href.startswith("/") or not href.startswith("http"): href = self.start_urls[0] + href
+
+            yield scrapy.http.Request(
+                    url=href,
+                    callback=self.parse_two,
+                    method="GET"
+                )
+
+        print(self.count)
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def parse_two(self, response):
+
+        self.count = self.count + 1
+        print(self.count)
+
+        sel = scrapy.Selector(response)
+        results = sel.xpath("//a[contains(@href, '')]/@href")
+        for result in results:
+            href = result.extract()
+            if href.startswith("https://facebook") \
+                    or href.startswith("https://apple"): continue
+            elif href.startswith("#"): continue
+            elif href.startswith("/"): href = self.start_urls[0] + href
+            # time.sleep(0.5)
+            yield scrapy.http.Request(
+                url=href,
+                callback=self.parse_two,
+                method="GET"
+            )
+
+#
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#
